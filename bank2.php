5e0f17dd637971f4081fad01100f747b783e1e65
rahul9409 <56967239+rahul9409@users.noreply.github.com>
2021-02-19 23:59:44 +0530

    Update bank2.php

--- bank2.php
+++ bank2.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+<body>
 <center>
-  <h1>Select And View one Customer</h1>
+  <h1 class="ab">Select And View one Customer</h1>
 <?php
-$conn = new mysqli("sql203.epizy.com", "epiz_27877403", "EBqe2t0NSFc", "epiz_27877403_banking");
-$query="select * from banktable";
+$conn = new mysqli("localhost", "root","" , "banking");
+$query="select * from customers";
 $result=$conn->query($query);?>
   <form class="" action="" method="post">
       <select name='ind'>
 <?php  while($row = $result->fetch_assoc()){?>
-    <option value="<?php  echo "Name:  " . $row["Name"] . " <br> Email:  " .$row["Email"]. " <br> CurrentBallance:  " .$row["CurrentBallance"]. " ";?>"> <?php  echo " " . $row["Name"] . " ";
+    <option id="dropdown" value="<?php  echo "Name:  " . $row["Name"] . " <br> Email:  " .$row["Email"]. " <br> CurrentBallance:  " .$row["CurrentBallance"]. " ";?>"> <?php  echo " " . $row["Name"] . " ";
   ?>
   <?php
   }
@@ -22,23 +23,43 @@ $result=$conn->query($query);?>
 <?php
   if(isset($_POST["submit"])){
   $value=$_POST['ind'];
+  ?>
+  <div class="abc">
+    <?php
   echo "Details <br>";
   echo($value);
-    $query1="SELECT Name, Email, CurrentBallance FROM banktable WHERE Name = '$value'";
-    $result = $conn->query($query1);
-      // output data of each row
-      while($row = $result->fetch_assoc()){
-        echo " Name: ".$row['Name']. " Email: ".$row['Email']. " CurrentBallance: ".$row['CurrentBallance']. "<br>";
 
 }
-}
  ?>
+</div>
  <br><br><br>
  <br>
  <?php if(isset($_POST["submit"])){ ?>
  <form class="" action="bank3.php" method="post">
- <button type="submit" name="button">Transfer Money</button>
+ <button class="sub" type="submit" name="button">Transfer Money</button>
  </form>
  <?php } ?>
 </center>
+</body>
+<style>
+body {
+  background-image: url('https://wallpapercave.com/wp/wp2561067.jpg');
+}
+#dropdown{
+  width: 1000px;
+ border-radius:1000px;
+ background-color:#D2B48C;
+}
+.abc{
+  color: #8B4513;
+}
+.ab{
+  color: #808000;
+}
+.sub{
+  background: #F0E68C;
+  width: 250px;
+  height: 40px;
+}
+</style>
  </html>
