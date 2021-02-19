332300fba9cc5d05411b72954d3595065a880797
rahul9409 <56967239+rahul9409@users.noreply.github.com>
2021-02-19 23:59:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,54 @@
 <!DOCTYPE html>
 <?php
-$conn = new mysqli("sql203.epizy.com", "epiz_27877403", "EBqe2t0NSFc", "epiz_27877403_banking");
+$conn = new mysqli("localhost", "root", "", "banking");
  ?>
 <html>
-<center>
-<h1> Home Page</h1>
-<br><br><br><br><br><br><br><br>
-<form class="" action="bank2.php" method="post">
-<button type="submit" name="button">view all customer</button>
-</form>
+<body>
+  <center>
+    <div class="abc">
+  <h1><strong>Basic Banking System</strong></h1>
+</div>
 </center>
+  <div class="topnav">
+  <a class= href="#">Contact</a>
+  <a href="bank2.php">view all customer</a>
+  <a href="#">About</a>
+  <a class="active" href="#">Home</a>
+</div>
+</body>
+<style>
+body {
+  background-image: url('https://devathon.com/wp-content/uploads/2020/02/Top-10-Payment-Gateways-Devathon.png');
+}
+.abc{
+  background-color: #FAEBD7;
+  color: #A9A9A9;
+}
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: right;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 20px 26px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
 </html>
